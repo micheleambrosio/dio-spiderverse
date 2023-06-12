ac59dd8755a1a8c9e9545de8fbdc7e93cd1b3e1f
micheleambrosio <micheleambrosio.dev@gmail.com>
2023-06-12 05:42:56 -0300

    Layout e cabeçalho do projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.scss";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata = {
   title: "Spiderverse",
@@ -11,11 +11,38 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body>
+        <header>
+          <Image
+            src="/icons/menu.svg"
+            alt="Menu options"
+            width={36}
+            height={25}
+            priority
+          />
+          <Link href="/">
+            <Image
+              src="/spider-logo.svg"
+              alt="Spiderman"
+              width={260}
+              height={70}
+              priority
+            />
+          </Link>
+          <Image
+            src="/icons/user.svg"
+            alt="Login"
+            width={36}
+            height={36}
+            priority
+          />
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }

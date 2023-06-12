0a1c0fa40af937593f8f81c336c28f887e6c6dcf
micheleambrosio <micheleambrosio.dev@gmail.com>
2023-06-12 06:37:27 -0300

    Criando o título "Personagens" ao fundo dos heróis

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,3 +1,6 @@
+import styles from "./heroesList.module.scss";
+
+import { spidermanFont } from "@/fonts";
 import { IHeroData } from "@/interfaces/heroes";
 
 interface IProps {
@@ -7,7 +10,9 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1>Personagens</h1>
+      <h1 className={`${spidermanFont.className} ${styles.title}`}>
+        Personagens
+      </h1>
     </>
   );
 }
---
+++ src/fonts/index.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const spidermanFont = localFont({
+  src: "../fonts/spider-man.otf",
+  weight: "400",
+  display: "swap",
+});

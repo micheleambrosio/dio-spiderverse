dce62c488ea9b307395ce7a801651a788a3744a1
micheleambrosio <micheleambrosio.dev@gmail.com>
2023-06-12 08:58:00 -0300

    Lista as imagens dos heróis do aranhaverso no componente Carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import HeroDetails from "../HeroDetails";
+import HeroPicture from "../HeroPicture";
 
 import styles from "./carousel.module.scss";
 
@@ -10,12 +15,43 @@ interface IProps {
 }
 
 export default function Carousel({ heroes, activeId }: IProps) {
+  // Controla os itens visíveis do carrossel
+  const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
+
+  // Armazena o item ativo do carrossel
+  const [activeIndex, setActiveIndex] = useState(
+    heroes.findIndex((hero) => hero.id === activeId)
+  );
+
+  // Altera o visibleItems sempre que o activeIndex é alterado
+  useEffect(() => {
+    // itens que serão mostrados ao longo do carrossel
+    const items = [...heroes];
+
+    // calcula o índice do array de acordo com o item ativo
+    // de forma que o número nunca saia do escopo do array
+    const indexInArrayScope =
+      ((activeIndex % items.length) + items.length) % items.length;
+
+    // itens que estão visíveis neste momento para o usuário
+    // duplicamos o array para dar a impressão de um carrossel infinito (360deg)
+    const visibleItems = [...items, ...items].slice(
+      indexInArrayScope,
+      indexInArrayScope + 3
+    );
+
+    setVisibleItems(visibleItems);
+  }, [heroes, activeIndex]);
+
   return (
     <div className={styles.container}>
       <div className={styles.carousel}>
         <div className={styles.wrapper}>
-          Área reservada para as imagens dos heróis com o efeito de carrossel
-          parallax
+          {visibleItems?.map((item) => (
+            <div key={item.id} className={styles.hero}>
+              <HeroPicture hero={item} />
+            </div>
+          ))}
         </div>
       </div>
       <div className={styles.details}>

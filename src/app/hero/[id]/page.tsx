018cdcbb706be5ccf520061a4f46528d17f92063
micheleambrosio <micheleambrosio.dev@gmail.com>
2023-06-12 08:13:56 -0300

    Chama o componente Carousel na nova página e envia os dados necessários

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -1,9 +1,24 @@
+import Carousel from "@/components/Carousel";
+import { IHeroData } from "@/interfaces/heroes";
+
 interface IProps {
   params: {
     id: string;
   };
 }
 
-export default function Hero({ params: { id } }: IProps) {
-  return <h1>Spider selecionado: {id}</h1>;
+async function getData(): Promise<{ data: IHeroData[] }> {
+  const res = await fetch("http://localhost:3000/api/heroes");
+
+  if (!res.ok) {
+    throw new Error("Falha ao buscar heróis");
+  }
+
+  return res.json();
+}
+
+export default async function Hero({ params: { id } }: IProps) {
+  const res = await getData();
+
+  return <Carousel heroes={res.data} activeId={id} />;
 }

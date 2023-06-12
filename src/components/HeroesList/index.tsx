797509d6a222cb7906cc195553c091acaa1d077b
micheleambrosio <micheleambrosio.dev@gmail.com>
2023-06-12 16:37:08 -0300

    Adiciona animação da página inicial

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 import HeroPicture from "../HeroPicture";
 
@@ -14,21 +17,34 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1 className={`${spidermanFont.className} ${styles.title}`}>
+      <motion.h1
+        className={`${spidermanFont.className} ${styles.title}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 2, duration: 2 }}
+      >
         Personagens
-      </h1>
-      <section className={styles.heroes}>
+      </motion.h1>
+      <motion.section
+        className={styles.heroes}
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
         {heroes.map((hero) => (
-          <div
+          <motion.div
             key={hero.id}
             className={`${styles.imageContainer} ${styles[hero.id]}`}
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.8 }}
+            transition={{ duration: 0.8 }}
           >
             <Link href={`/hero/${hero.id}`}>
               <HeroPicture hero={hero} />
             </Link>
-          </div>
+          </motion.div>
         ))}
-      </section>
+      </motion.section>
     </>
   );
 }

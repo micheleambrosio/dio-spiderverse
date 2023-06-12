4c1934a1882c086ba30e89772ac738f2df01b8e9
micheleambrosio <micheleambrosio.dev@gmail.com>
2023-06-12 08:37:03 -0300

    Estrutura as áreas do componente Carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,3 +1,7 @@
+import HeroDetails from "../HeroDetails";
+
+import styles from "./carousel.module.scss";
+
 import { IHeroData } from "@/interfaces/heroes";
 
 interface IProps {
@@ -6,5 +10,17 @@ interface IProps {
 }
 
 export default function Carousel({ heroes, activeId }: IProps) {
-  return <h1>Componente Carousel</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.carousel}>
+        <div className={styles.wrapper}>
+          Área reservada para as imagens dos heróis com o efeito de carrossel
+          parallax
+        </div>
+      </div>
+      <div className={styles.details}>
+        <HeroDetails data={heroes[0]} />
+      </div>
+    </div>
+  );
 }
